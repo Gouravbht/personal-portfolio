ca9ae4409394294d47f61588a1af46d546131b53
Gourav Bhati <gouravb791@gmail.com>
2023-02-24 23:52:39 +0530

    site init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#181818",
+        secondary: "white",
+        accent: "#4B56D2",
+      },
+    },
+    plugins: [],
+  },
+};
