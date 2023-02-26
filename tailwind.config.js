717f309fa2ca7b337f18d067497d87804401ddf1
Gourav Bhati <gouravb791@gmail.com>
2023-02-26 18:39:01 +0530

    analytics added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["*", "node_modules/preline/dist/*.js"],
   theme: {
     extend: {
       colors: {
-        primary: "#181818",
+        primary: "rgb(4,7,47)",
         secondary: "white",
         accent: "#4B56D2",
       },
     },
-    plugins: [],
+    plugins: [require("preline/plugin")],
   },
 };
